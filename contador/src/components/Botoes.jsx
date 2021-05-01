df9c19f1de7c3b7ceb5d71e96a5926b608e3c936
PaolaRampazzo <paolatrampazzo@gmail.com>
2021-05-01 20:06:23 +0000

    Aula 03 - Contador
    Signed-off-by: PaolaRampazzo <paolatrampazzo@gmail.com>

---
+++ contador/src/components/Botoes.jsx
@@ -0,0 +1,10 @@
+import Raect from 'react'
+
+export default props => {
+    return(
+        <div>
+            <button onClick={props.setInc} >+</button>
+            <button onClick={props.setDec} >-</button>
+        </div>
+    )
+}
